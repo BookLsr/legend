db563af90fcbb0867bfd1303e6389cb3620b1960
WindPursuer <wang.tb@neusoft.com>
2017-08-10 15:25:36 +0800

    初始化

--- legend-manage/src/main/java/com/wang/Application.java
+++ legend-manage/src/main/java/com/wang/Application.java
@@ -1,34 +1,34 @@
 package com.wang;
 
-import org.springframework.boot.SpringApplication;
+/*import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;*/
 
 /**
  * Description legend后台管理启动类
  * @Author windpursuer
  * @Date 2017/8/10 10:20
  */
-@SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+/*@SpringBootApplication*/
+public class Application {
     /**
      * Description 使用外部tomcat
      * @Author windpursuer
      * @Date 2017/8/10 14:34
      */
-    @Override
+  /*  @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         builder.sources(this.getClass());
         return super.configure(builder);
     }
 
-    /**
+    *//**
      * Description springboot启动主函数
      * @Author windpursuer
      * @Date 2017/8/10 10:55
-     */
+     *//*
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
-    }
+    }*/
 }
---
+++ legend-manage/src/main/java/com/wang/config/MybatisConfig.java
@@ -0,0 +1,11 @@
+package com.wang.config;
+
+
+/**
+ * Description Mybatis配置类
+ * @Author windpursuer
+ * @Date 2017/8/10 15:00
+ */
+@Configuration
+public class MybatisConfig {
+}
