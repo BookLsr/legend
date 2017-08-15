e36c943a1cee9be7f225d34c5c11326b8e44ff80
WindPursuer <wang.tb@neusoft.com>
2017-08-15 13:57:22 +0800

    初始化

--- legend-service/src/main/java/com/wang/service/impl/HeroDetailServiceImpl.java
+++ legend-service/src/main/java/com/wang/service/impl/HeroDetailServiceImpl.java
@@ -6,6 +6,7 @@ import com.wang.service.HeroDetailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,6 +19,9 @@ public class HeroDetailServiceImpl implements HeroDetailService {
     private Logger logger = LoggerFactory.getLogger(HeroDetailServiceImpl.class);
     @Autowired
     private HeroDetailDao heroDetailDao;
+
+    @Value("${param}")
+    private String param;
     /**
      * Description 根据ID获取用户信息
      * @param id
@@ -27,7 +31,7 @@ public class HeroDetailServiceImpl implements HeroDetailService {
     @Override
     public HeroDetailEntity getHeroDetailById(int id) {
         HeroDetailEntity heroDetailEntity = heroDetailDao.getHeroDetailById(1);
-        logger.debug("根据ID查询英雄信息如下：【{}】", heroDetailEntity.toString());
+        logger.debug("参数：【{}】,根据ID查询英雄信息如下：【{}】", param, heroDetailEntity.toString());
         return heroDetailEntity;
     }
 }
